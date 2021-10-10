dba194cc692dc20085142b82b795a41634a20cdf
vgingupa <91331842+vgingupa@users.noreply.github.com>
2021-10-10 00:16:25 -0500

    clean code`

--- test4.py
+++ test4.py
@@ -4,11 +4,6 @@ from azure.keyvault.secrets import SecretClient
 from azure.mgmt.compute import ComputeManagementClient
 
 
-#AZURE_CLIENT_ID="528c082f-4bfd-4941-ae6e-66b59a78c920"
-#AZURE_CLIENT_SECRET="dZF1d~BUVI.cnHQGw58fw8pFevfR~e19YI"
-#AZURE_SUBSCRIPTION_ID="0266d00b-f7f1-40cd-b895-35e76be40442"
-#AZURE_TENANT_ID="2b65c7f1-1021-4303-8c3c-3e0392c68a96"
-#KEYVAULT_NAME="Pythonkeyvalut"
 AZURE_CLIENT_ID=env.get("AZURE_CLIENT_ID", "")
 AZURE_CLIENT_SECRET=env.get("AZURE_CLIENT_SECRET", "")
 AZURE_SUBSCRIPTION_ID=env.get("AZURE_SUBSCRIPTION_ID", "")
